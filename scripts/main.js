4904eb6caf3eded0e87b1229cd78987a6ffc8eff
Jesper <jesper.olsson@iths.se>
2021-10-31 20:04:42 +0100

    added loading icon, planet info, pagination
    Co-authored-by: OscarArr <OscarArr@users.noreply.github.com>

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,38 @@
-renderCharacters = () => {
+renderCharacters = (counter) => {
   const characterList = document.querySelector(".characters");
-  fetch(`https://swapi.dev/api/people`)
-    .then((response) => response.json())
-    .then((data) => {
+  if (counter === undefined) {
+    counter = 1
+  }
+  
+  characterList.innerHTML = '<div class="lds-dual-ring"></div>'
+  fetch(`https://swapi.dev/api/people/?page=${counter}`)
+  .then((response) => response.json())
+  .then((data) => {
+      characterList.innerHTML = ""
       for (let char of data.results) {
         characterList.insertAdjacentHTML("beforeend", `
             <li>${char.name}</li>`)
-            document.querySelector("li:last-of-type").addEventListener("click", () => {
-                renderCharInfo(char)
-            })
+        document.querySelector("li:last-of-type").addEventListener("click", (e) => {
+          // console.log(e)
+          // e.target.insertAdjacentHTML("beforeend", `
+          // <span>&#9657</span>
+          // `)
+          renderCharInfo(char)
+        })
 
-        }
-    });
+      }
+    })
+
 };
 
 renderCharInfo = (char) => {
-    const charContainer = document.querySelector(".charDetails")
-    charContainer.innerHTML = ""
-    charContainer.insertAdjacentHTML("beforeend", `
+  const charContainer = document.querySelector(".charDetails")
+  charContainer.innerHTML = '<div class="lds-dual-ring"></div>'
+  const container = document.querySelector(".planetInfo")
+  charContainer.innerHTML = ""
+  charContainer.insertAdjacentHTML("beforeend", `
     <h3>${char.name}</h3>`)
-    charContainer.insertAdjacentHTML("beforeend", `
+  charContainer.insertAdjacentHTML("beforeend", `
     <li>Height: ${char.height}cm</li>
     <li>Mass: ${char.mass}kg</li>
     <li>Hair color: ${char.hair_color}</li>
@@ -28,10 +41,53 @@ renderCharInfo = (char) => {
     <li>Birth Year: ${char.birth_year}</li>
     <li>Gender: ${char.gender}</li>
     `)
-    document.querySelector(".planetInfo").insertAdjacentHTML
+
+  container.innerHTML = '<div class="lds-dual-ring"></div>'
+  fetch(char.homeworld).then((response) => response.json()).then((data => {
+    container.innerHTML = ""
+    container.insertAdjacentHTML("beforeend", `
+      <h3>${data.name}</h3>
+      <li>Rotation period: ${data.rotation_period}</li>
+      <li>Orbital period: ${data.orbital_period}</li>
+      <li>Diameter: ${data.diameter}</li>
+      <li>Climate: ${data.climate}</li>
+      <li>Gravity: ${data.gravity}</li>
+      <li>Terrain: ${data.terrain}</li>      
+      `)
+  }))
 }
 
+paginator = () => {
+  let counter = 1
+  const numberList = document.querySelector(".numberList")
+  const nextBtn = document.querySelector(".nextBtn")
+  const prevBtn = document.querySelector(".prevBtn")
+  nextBtn.addEventListener("click", () => {
+    counter++
+    numberList.innerText++
+    if (counter >= 9) {
+      nextBtn.style.visibility = "hidden"
+    }else {
+      prevBtn.style.visibility = "visible"
+    }
+    renderCharacters(counter)
+  })
+  
+  
+  prevBtn.addEventListener("click", () => {
+    counter--
+    numberList.innerText--
+    if (counter <= 1){
+      prevBtn.style.visibility = "hidden"
+    }
+    else{ 
+      nextBtn.style.visibility = "visible"
+    }
+    renderCharacters(counter)
+  })
+}
 main = () => {
-  renderCharacters();
+  renderCharacters()
+  paginator()
 };
 main();
